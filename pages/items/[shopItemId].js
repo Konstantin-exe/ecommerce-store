7bd2300e53a37527491cda3cf3ea9255d51f646b
Konstantin-exe <konstantinmuellner@gmail.com>
2021-02-18 17:32:38 +0100

    item landing page done, enhanced store page

--- pages/items/[shopItemId].js
+++ pages/items/[shopItemId].js
@@ -1,6 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import Link from 'next/link';
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import { getItemInfo } from '../../utils/database';
+import styles, {
+  backToStoreButton,
+  singleItemPage,
+  singleItemPageBuyField,
+} from '../../styles/styles';
 
 export default function ShopItem(props) {
   return (
@@ -9,9 +17,21 @@ export default function ShopItem(props) {
         <title>{props.shopItem.itemName}</title>
       </Head>
       <h1>{props.shopItem.itemName}</h1>
-      <img src={props.shopItem.imgUrl} alt={props.shopItem.itemName} />
-      <p>{props.shopItem.longDescription}</p>
-      <button>Add Item To Cart</button>
+      <Link href={`/items`}>
+        <button css={backToStoreButton} href={`/items`}>
+          Back to store
+        </button>
+      </Link>
+      <div css={singleItemPage}>
+        <img src={props.shopItem.imgUrl} alt={props.shopItem.itemName} />
+        <div css={singleItemPageBuyField}>
+          <a>{props.shopItem.price} SMH</a>
+          <br />
+          <a>Free delivery within Dimension C-137</a>
+          <button>Add Item To Cart</button>
+        </div>
+        <p>{props.shopItem.longDescription}</p>
+      </div>
     </Layout>
   );
 }
