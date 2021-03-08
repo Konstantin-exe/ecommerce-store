61aabfc4790778cc6e3819c7f0f762934269865d
Konstantin-exe <konstantinmuellner@gmail.com>
2021-03-08 15:21:46 +0100

    updated database for heroku

--- utils/database.js
+++ utils/database.js
@@ -9,7 +9,10 @@ function connectOneTimeToDatabase() {
   let sql;
 
   if (process.env.NODE_ENV === 'production') {
-    sql = postgres({ rejectUnauthorized: false });
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
   } else {
     if (!globalThis.__postgresSqlClient) {
       globalThis.__postgresSqlClient = postgres();
